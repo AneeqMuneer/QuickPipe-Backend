0444a4af89787b6e3219ccaf3639e3eddd46d7d2
Aneeq Muneer <aneeq.muneer03@gmail.com>
2025-03-17 18:10:39 +0500

    smaol changes

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route("/Signup").post(Signup);
 router.route("/Login").post(Login, TwoFactorAuthentication);
-router.route("/ForgetPassword").put(VerifyUser, ForgetPassword);
+router.route("/ForgetPassword").put(ForgetPassword);
 
 router.route("/VerifyCode").post(VerifyCode);
 router.route("/2FA").get(VerifyUser, TwoFactorAuthentication);
