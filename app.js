33f5a9c5361436bbfed965a74d913e2388454291
Ahad Aziz Jaffer <163832053+ahadazizjaffer2@users.noreply.github.com>
2025-03-28 00:42:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 app.use(cors({ 
     origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
