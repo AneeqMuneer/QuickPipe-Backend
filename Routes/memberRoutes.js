2a511ff68f4a8b6e477827ca3cdaf553108564bb
Aneeq Muneer <aneeq.muneer03@gmail.com>
2025-04-06 16:18:32 +0500

    params

--- Routes/memberRoutes.js
+++ Routes/memberRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route("/AddMember").post(VerifyUser, AddMember);
 router.route("/GetWorkspaceMembers").get(VerifyUser , GetWorkspaceMembers);
-router.route("/VerifyInvitation").get(VerifyInvitation);
+router.route("/VerifyInvitation/:wkid/:usid").get(VerifyInvitation);
 router.route("/AcceptInvitation").put(AcceptInvitation);
 router.route("/RejectInvitation").put(RejectInvitation);
 
