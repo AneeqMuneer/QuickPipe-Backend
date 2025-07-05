efb55115bd01c9e2bf78d9f09c5b73b2ed444f6f
Aneeq Muneer <aneeq.muneer03@gmail.com>
2025-07-06 00:58:48 +0500

    fixed webhook live feed issue

--- Routes/dashboardRoutes.js
+++ Routes/dashboardRoutes.js
@@ -5,7 +5,7 @@ const { VerifyUser } = require("../Middleware/userAuth");
 const router = express.Router();
 
 router.route("/SummaryWidget").get(VerifyUser, SummaryWidget);
-router.route("/LiveFeedWidget").post(VerifyUser, LiveFeedWidget);
+router.route("/LiveFeedWidget").post(LiveFeedWidget);
 router.route("/StatsWeeklyWidget").get(VerifyUser, StatsWeeklyWidget);
 router.route("/StatsMonthlyWidget").get(VerifyUser, StatsMonthlyWidget);
 router.route("/TopPeopleWidget").get(VerifyUser, TopPeopleWidget);
